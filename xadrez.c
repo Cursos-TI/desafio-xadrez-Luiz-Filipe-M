f461b716d5d425c00bab8afc17ca77c179ac5815
Luiz-Filipe-M <lipe1997.lupero@gmail.com>
2025-09-30 20:53:02 -0300

    Update xadrez.c
    Adicionei novos testes

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-   printf("Desafio xadrez!\n");
-    return 0;
+int main(void) {
+  int idade = 28;
+  float altura = 1.73;
+  char sexo = 'M';
+
+  printf("Idade: %d\nAltura: %f\nSexo: %c\n", idade, altura, sexo);
+
+  return 0;
 }
